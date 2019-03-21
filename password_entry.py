5fbd7c6d10e692dfe631f912a5d156ae76882631
jc462814 <Rmkm7799>
2019-03-21 13:12:09 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Kyle"""
+
+MIN_LEN = 6
+
+password = input("Enter password: ")
+while len(password) < MIN_LEN:
+    print("Your password isn't long enough")
+    password = input("Enter password: ")
+
+print("*"*len(password))
